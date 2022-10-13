b056af437fa95fbf36bad29516036e43d7b0f666
Mohammad-Daghash <zzazza0101@gmail.com>
2022-10-13 07:29:08 +0300

    Testing...

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -63,7 +63,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async session => {
     const tour = session.client_reference_id;
     const user = (await User.findOne({ email: session.customer_email })).id;
-    const price = session.display_items[0].amount / 100;
+    const price = session.amount_total / 100;
     await Booking.create({ tour, user, price });
 };
 
@@ -82,7 +82,6 @@ exports.webhookCheckout = (req, res, next) => {
         return res.status(400).send(`Webhook error: ${err.message}`);
     }
     if (event.type === 'checkout.session.completed') {
-        console.log(event.type);
         createBookingCheckout(event.data.object);
     }
 
